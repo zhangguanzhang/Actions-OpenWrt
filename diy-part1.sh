5503698902db9425588f698ccbe4c60f103f03de
zhangguanzhang <zhangguanzhang@qq.com>
2021-08-15 01:36:13 +0800

    add,update config
    Signed-off-by: zhangguanzhang <zhangguanzhang@qq.com>

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,20 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+#sed -i 's/192.168.1.1/192.168.100.254/g' package/base-files/files/bin/config_generate
+sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_generate
+sed -i "/timezone='CST-8'/a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
+echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+echo "src-git others https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+
+# #OpenClash
+# git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git package/new/luci-app-openclash
--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,13 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-ifit/g' feeds/luci/collections/luci/Makefile
+
+# luci-theme-atmaterial_new
+git clone -b main --depth 1 https://github.com/Chandler-Lu/openwrt-package /tmp/openwrt-package
+if [ -d '/tmp/openwrt-package/luci-theme-atmaterial_new' ];then
+    mv /tmp/openwrt-package/luci-theme-atmaterial_new package/new/
+    echo 'CONFIG_PACKAGE_luci-theme-atmaterial_new=y' >> ${CUSTOME_CONFIG}
+fi
